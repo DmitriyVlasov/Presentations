5f621bc106a5eb322bfcff1360b6d6e4411ece2d
Dmitriy Vlasov <vlasovde@gmail.com>
2015-08-23 18:30:43 +0300

    Создана индексная страница
    Индесная страница будет корневой для списка всех презентаций выложенных
    на github.io

---
+++ slides/index.fsx
@@ -0,0 +1,13 @@
+﻿(**
+- title : List of presentations
+- description : Список публичных презентаций Дмитрия Власова
+- author : Dmitriy Vlasov
+- theme : night
+- transition : default
+*)
+
+(*****
+
+### Список презентаций
+
+* [Обзор F# 4.0](http://dmitriyvlasov.github.io/Presentations/review-fsharp-4.html#)*)
