02bd60ff91ce4816371073768d0c4d2cb3e4c3d6
Dmitriy Vlasov <vlasovde@gmail.com>
2017-08-17 11:31:43 +0300

    Обновлено содержание презентаций.

--- slides/index.fsx
+++ slides/index.fsx
@@ -11,5 +11,9 @@
 ### Список презентаций
 
 * Обзор F# 4.0 ([слайды](http://dmitriyvlasov.github.io/Presentations/review-fsharp-4.html) и [видео](https://youtu.be/iOItE4hR6jg)).
-</br>_25 августа 2015, [Saint-Peterburg .NET Community](https://plus.google.com/u/0/communities/115643868183582812348)_
+  * 25 августа 2015
+  * [Saint-Peterburg .NET Community](https://plus.google.com/u/0/communities/115643868183582812348).
+* Разработка пользовательских функций в Power Query ([слайды](http://dmitriyvlasov.github.io/Presentations/writing-custom-functions-in-power-query.html#/)).
+  * 23 августа 2017
+  * [Saint-Peterburg Power BI User Group](https://www.pbiusergroup.com/communities/community-home?CommunityKey=f6f83caa-78fb-4a11-92fb-522457064f77).
 *)
