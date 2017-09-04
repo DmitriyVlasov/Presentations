7be1c305be3de8f177e544b8c3e0ac6fef3067ff
Dmitriy Vlasov <vlasovde@gmail.com>
2017-09-04 12:13:41 +0300

    Добавит ссылку на видео с Power BI митапа.

--- slides/index.fsx
+++ slides/index.fsx
@@ -12,7 +12,7 @@
 
 * Обзор F# 4.0 ([слайды](http://dmitriyvlasov.github.io/Presentations/review-fsharp-4.html) и [видео](https://youtu.be/iOItE4hR6jg)).
   * 25 августа 2015, [Saint-Peterburg .NET Community](https://plus.google.com/u/0/communities/115643868183582812348).
-* Разработка пользовательских функций в Power Query ([слайды](http://dmitriyvlasov.github.io/Presentations/writing-custom-functions-in-power-query.html#/)).
+* Разработка пользовательских функций в Power Query ([слайды](http://dmitriyvlasov.github.io/Presentations/writing-custom-functions-in-power-query.html#/) и [видео](https://youtu.be/1Vb89AS2g2U)).
   * 23 августа 2017, [Saint-Peterburg Power BI User Group](https://www.pbiusergroup.com/communities/community-home?CommunityKey=f6f83caa-78fb-4a11-92fb-522457064f77);
   * 31 августа 2017, [FProg Spb](https://spb-fp-meetup.timepad.ru/events/).
 *)
